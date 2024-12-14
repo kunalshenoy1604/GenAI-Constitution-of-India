41cc4ccea5fb17285d3c3f66b649b1ca67000efd
kunalshenoy1604 <shenoykunal1309@gmail.com>
2024-12-14 16:05:02 +0530

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="GenAI Project",
+    version='0.0.1',
+    author='Kunal Shenoy',
+    author_email='shenoykunal1309@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
